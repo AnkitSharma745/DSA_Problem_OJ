c9c8984a942cc4b6622512fd6098e1526b371af0
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-02 18:32:13 +0530

    Done

---
+++ maximum_occurence_elements.js
@@ -0,0 +1,22 @@
+function maximumOccuringElement(A,N){
+  let obj={}
+  for(let i=0;i<N;i++){
+      if(obj[A[i]]==undefined){
+          obj[A[i]]=0
+      }
+      else{
+          obj[A[i]]++
+      }
+  }
+  
+  let max= -Infinity
+  let temp=""
+  for(let key in obj){
+      if(obj[key]>max){
+          max=obj[key]
+          temp=key
+      }
+      
+  }
+  console.log(temp)
+}
