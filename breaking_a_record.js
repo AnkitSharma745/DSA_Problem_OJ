f77a6407a29f7f59de0f588e21d56b638e9e7e4c
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-02 23:44:51 +0530

    Done

---
+++ breaking_a_record.js
@@ -0,0 +1,13 @@
+function breakingTheRecord(n, m) {
+  if(n==m){
+      console.log("Wao")
+  }
+  else if(n>m){
+      console.log("Not Yet")
+  }
+  else{
+      console.log("Broken")
+  }
+  
+  
+  }
