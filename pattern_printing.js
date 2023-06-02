0e1220977073dfbc9aae92cd0c512327aeeedddd
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-02 23:50:13 +0530

    Done

---
+++ pattern_printing.js
@@ -0,0 +1,12 @@
+function patternPrinting(N) {
+  // Write code here
+  for(let i=0;i<N;i++){
+  let bag=""
+      for(let j=0;j<=i;j++){
+          bag+="*"
+      }
+  
+console.log(bag)
+  }
+
+}
