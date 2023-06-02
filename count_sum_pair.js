78a27274c4bfb9182a98d86ad43ac5043c11d366
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-02 16:29:44 +0530

    Done

---
+++ count_sum_pair.js
@@ -0,0 +1,11 @@
+function countSuchPairs(N,K,A){
+  //write code here
+  let count=0
+  for(let i=0;i<N;i++){
+      for(let j=i+1;j<N;j++){
+          if(A[i] + A[j]== K){
+              count++
+          }        }
+  }
+  console.log(count)
+}
