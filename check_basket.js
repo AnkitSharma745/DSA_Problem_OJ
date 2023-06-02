c8b293a7e117170e203912acc07e3e9cb8b08566
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-02 19:01:14 +0530

    Done

---
+++ check_basket.js
@@ -0,0 +1,12 @@
+function checkBasket(N,B){
+  // write code here
+  if(B.length==1){
+      console.log("Single Type")
+  }
+  else if(B[0]==B[1]){
+      console.log("Single Type")
+  }
+  else{
+      console.log("Mixed Basket")
+  }
+}
