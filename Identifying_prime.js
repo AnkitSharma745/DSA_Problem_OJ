4526fb8473a16eeb99818723b818d2a11e3742d9
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-02 18:56:18 +0530

    Done

---
+++ Identifying_prime.js
@@ -0,0 +1,15 @@
+function identifyPrime(num) {
+  // Write code here
+  let count=0
+  for(let i=0;i<=num;i++){
+      if(num%i==0){
+          count++
+      }
+  }
+  if(count==2){
+      console.log("Yes")
+  }
+  else{
+      console.log("No")
+  }
+}
