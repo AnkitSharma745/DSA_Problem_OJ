7b566ae8f0d7677719920f095702a9e8072c4d19
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-03 22:14:13 +0530

    Masai palinromic string has been done

---
+++ masai_palindromic_string.js
@@ -0,0 +1,24 @@
+function masaiPalSubString(s){
+  //write code here
+   let   max= -Infinity
+  for(let i=0;i<s.length;i++){
+      let str=""
+      for(let j=i;j<s.length;j++){
+      str+=s[j]
+        if(pal(str)>max){
+            max=pal(str)
+        }
+    
+      }
+  }
+console.log(max)
+  function pal(char){
+  let str=""
+  for(let i=char.length-1;i>=0;i--){
+      str+=char[i]
+  }
+      if(str==char){
+          return char.length
+      }
+  }
+}
