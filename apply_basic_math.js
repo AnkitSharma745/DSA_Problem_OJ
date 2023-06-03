a54fbea51fe53620cd0ab9d748c7f3cedbb620b5
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-06-04 01:35:15 +0530

    Done

---
+++ apply_basic_math.js
@@ -0,0 +1,17 @@
+function applyBasicMaths(N,A){
+  let min = Infinity
+  let index=-1
+  for(let i=0;i<N;i++){
+      let sum=0
+      for(let j=0;j<N;j++){
+          if(i!=j){
+              sum+=A[j]
+          }
+      }
+      if(sum%7==0 && min>A[i]){
+          min=A[i]
+          index=i
+      }
+  }
+  console.log(index)
+}
